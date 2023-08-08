7fa79f3e8b54a8263b64f92ff61d44d78d8d0eac
Euller Maso <euller.maso@gmail.com>
2023-08-08 10:10:05 -0300

    feat: fetch tasks from back-end; clean useless files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 
 //useState é utilizado para mudar o estado de uma variável (mudar o valor)
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useState} from 'react'
+import axios from 'axios'
+
 import TaskItem from './components/TaskItem';
-import PaisesItem from './components/PaisesItem';
-import Cidades from './components/Cidades';
+
 
 const App = () => {
 
-  const mounted = useRef(false);
-
-    if(mounted.current === false){
-      mounted.current = true
-    }else{
-      console.log("component was updated")
-    }
 
   //Criando uma lista de tarefas
   //O useState é necessário para alterar o virtual DOM
@@ -31,35 +25,20 @@ const App = () => {
   },
 ])
 
-const [paises, setPaises] = useState([
-  {
-    id: 1,
-    name: "Brasil",
-    isBig: true
-  },
-  {
-    id: 2,
-    name: "Colombia",
-    isBig: false
-  }
-]);
 
-  const handleCleanTasks = () => {
-    setTasks([])
+  const fetchTasks = async () => {
+    try{
+      const {data} = await axios.get('http://localhost:8000/tasks')
+      setTasks(data)
+      
+    }catch(error){
+      console.log(error)
+    }
   }
 
-const [cidades, setCidade] = useState([
-  {
-    id: 1,
-    name: "SP",
-    region: "Sudeste"
-  },
-  {
-    id: 2,
-    name: "Amazonas",
-    region: "Norte"
-  }
-])
+  useEffect(() => {
+    fetchTasks();
+  },[])
 
   
   //Renderizando 1 item pra cada tarefa
@@ -68,14 +47,6 @@ const [cidades, setCidade] = useState([
     
       {tasks.map((task) => 
         (<TaskItem key={task.id} task={task}/>))} 
-
-      {paises.map((paises) => (<PaisesItem key={paises.id} paises={paises}></PaisesItem>))}
-
-      {cidades.map((cidades) => (<Cidades key={cidades.id} city={cidades}></Cidades>))}
-
-      <button onClick={handleCleanTasks}>Limpar task</button>
-      
-    
     
     </>
   );
@@ -83,60 +54,3 @@ const [cidades, setCidade] = useState([
 
 export default App;
 
-/*import React from 'react'
-import TaskItem from './components/TaskItem'
-
-class App extends React.Component{
-    constructor(props){
-      //Passar o super para chamar o constructor da classe React.Component e passar o super
-      super(props)
-      //atribuindo o this.handleSetState para o método que foi criado, pois ao colocar "this." dentro do onClick ele será referenciado pelo this do onClick por ser uma função
-      this.handleSetState = this.handleSetState.bind(this)
-      //Lembrando que o this se refere ao React.Component
-
-
-      this.state = {
-        tasks: [
-          {
-            id: 1,
-            description: 'Estudar Programação',
-            isCompleted: false
-      
-        },
-        {
-          id: 2,
-          description: "ler",
-          isCompleted: true,
-        },
-      ]
-
-      }
-    }
-
-    componentDidUpdate(prevProps, prevState){
-      console.log(prevState)
-      console.log(prevProps)
-      console.log("component was updated!")
-    }
-
-    handleSetState(){
-      this.setState({
-        tasks: []
-      })
-    }
-
-    //A função render é utilizada para rederizar nossa aplicação
-    render(){
-      return(
-        <>
-        {this.state.tasks.map((task) => 
-        (<TaskItem key={task.id} task={task}/>))} 
-
-        <button onClick={this.handleSetState}>Change State</button>
-        
-        </>
-      )
-    }
-};
-
-export default App;*/
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,20 +1,4 @@
-import { useEffect } from "react";
-
 const TaskItem = ({task}) => {
-    
-
-    //Maneira de utilizar componentDidMount e componentWillUnmount 
-    useEffect(() => {
-        console.log("component was mounted!")
-
-        return () => {
-            console.log("I will unmount!")
-        };
-        //Essas [] são utilizadas para indicar que um state foi mudado. Seria necessário passar o state dentro da []
-    },[])
-    
-    
-    
     return (
 
         <> 
@@ -22,39 +6,7 @@ const TaskItem = ({task}) => {
             <p>{task.isComleted ? 'Completa' : 'Não Completa'}</p>
         </>
     )
-    
-    
 };
 
 export default TaskItem;
 
-/*//Componenetes de classe (Não é a melhor forma)
-
-import React from "react";
-
-//O React.component nos dará uma função chamada render()
-class TaskItem extends React.Component{
-    componentDidMount(){
-        console.log("component was mounted")
-    }
-
-    componentWillUnmount(){
-        console.log("i will unmount")
-    }
-
-
-
-    render(){
-        //Fazendo o destructing da props para uma melhor sintaxe
-        const {task} = this.props
-        return (
-            <>
-            <h1>{task.description}</h1>
-            <p>{task.isCompleted ? 'Completa' : "Não Completa"}</p>
-            
-            </>
-        )
-    }
-}
-
-export default TaskItem;*/
