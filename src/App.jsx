339033d535ea1e042eee357e3c660c5995f5486b
Euller Maso <euller.maso@gmail.com>
2023-08-09 17:28:31 -0300

    feat: add Task item component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Tasks from "./components/Tasks";
+
+const App = () => {
+
+
+  
+  //Renderizando 1 item pra cada tarefa
+  return (
+    <>
+      <Tasks/>
+    
+    
+    </>
+  );
+}
+
+export default App;
+
