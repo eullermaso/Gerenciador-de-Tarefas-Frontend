866961d2187090446fc5016b099609f4159dabc9
Euller Maso <euller.maso@gmail.com>
2023-08-17 13:04:06 -0300

    feat: add ract-alert setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 2500,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //A função render é utilizada para colocar os import na tela
 root.render(
+  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+    </AlertProvider>
+  </React.StrictMode>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
