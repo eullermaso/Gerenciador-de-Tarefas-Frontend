a5482d7c02699f974eff8adb3efe414023c3324c
Euller Maso <euller.maso@gmail.com>
2023-08-17 16:08:38 -0300

    feat: add app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Tasks from "./components/Tasks";
 import Sidebar from './components/Sidebar'
 
+import "./App.scss"
+
+
 const App = () => {
-
-
-  
   //Renderizando 1 item pra cada tarefa
   return (
-    <>
+    <div className="app-container">
       <Sidebar/>
         <Tasks/>
-    </>
+    </div>
   );
 }
 
